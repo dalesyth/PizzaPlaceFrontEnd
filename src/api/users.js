e2d8dfbeda6bec62135a2766ceabce201d3c7a26
Dale Syth <dalesyth@gmail.com>
2023-10-15 18:15:01 -0500

    added login api endpoint

--- src/api/users.js
+++ src/api/users.js
@@ -14,3 +14,16 @@ export const registerUser = async (firstName, lastName, email, password) => {
     console.error("Error registering new user: ", error);
   }
 };
+
+export const loginUser = async (email, password) => {
+    try {
+        const response = await axiosInstance.post("/users/login", {
+            email: email,
+            password: password,
+        });
+        console.log("response.data from loginUser function: ", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error logging in user: ", error)
+    }
+}
