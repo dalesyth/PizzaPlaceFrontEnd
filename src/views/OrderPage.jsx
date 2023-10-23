21069ce5658f98b82768664057e4231815609cf4
Dale Syth <dalesyth@gmail.com>
2023-10-22 22:25:17 -0500

    working on OrderPage

--- src/views/OrderPage.jsx
+++ src/views/OrderPage.jsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { getAllSpecialtyPizzas } from "../api/specialtypizzas";
+import useAuth from "../hooks/useAuth";
 
 const OrderPage = () => {
   const [pizzas, setPizzas] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
+
+  const { auth } = useAuth()
+
+  
+  
 
   useEffect(() => {
     const getPizzas = async () => {
@@ -21,6 +28,19 @@ const OrderPage = () => {
     getPizzas();
   }, []);
 
+ 
+
+ 
+
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleAddToCart = async () => {
+    const email = auth.email
+    console.log("email from handleAddToCart: ", email)
+  }
+
   return (
     <>
       <div>
@@ -33,29 +53,58 @@ const OrderPage = () => {
       </div>
       <div>
         {pizzas.map((pizza, pizzaIndex) => (
-          <div key={pizzaIndex} className="bg-green-100 mb-2 p-2">
-            <div className="flex justify-between font-bold">
+          <div
+            key={pizzaIndex}
+            className="border-black border-2 rounded-lg shadow-lg mb-2 p-2"
+          >
+            <div className="flex justify-between font-bold text-sm lg:text-base">
               <span>{pizza.pizzaName}</span>
               <span>{pizza.pizzaPrice}</span>
             </div>
 
             <div>
-              <div>
+              <div className="text-xs lg:text-base">
                 <span className="mr-2">Toppings:</span>
-                <span>
+                <p>
                   {pizza.toppings.map((topping, toppingIndex) => (
                     <span key={toppingIndex}>{topping.toppingName}</span>
                   ))}
-                </span>
+                </p>
               </div>
 
-              <div>
-                <span className="mr-2">Sauce:</span>
-                <span>{pizza.sauceName}</span>
-              </div>
-              <div>
-                <span className="mr-2">Crust:</span>
-                <span>{pizza.crustName}</span>
+              <div className="flex justify-between">
+                <span>
+                  <div className="text-xs lg:text-base">
+                    <span className="mr-2">Sauce:</span>
+                    <span>{pizza.sauceName}</span>
+                  </div>
+                  <div className="text-xs lg:text-base">
+                    <span className="mr-2">Crust:</span>
+                    <span>{pizza.crustName}</span>
+                  </div>
+                </span>
+                <span>
+                  <div>
+                    <label className="mr-2 text-xs lg:text-base" htmlFor="quantity">
+                      Quantity:
+                    </label>
+                    <input
+                      className="w-12 text-center text-xs lg:text-base"
+                      type="number"
+                      id="quantity"
+                      value={quantity}
+                      onChange={handleQuantity}
+                    ></input>
+                  </div>
+                  <div className="text-right">
+                    <button
+                      className="bg-blue-400 text-white text-xs lg:text-base font-bold px-0.5 py-1 mt-2 rounded-lg hover:bg-blue-600 hover:font-extrabold"
+                      onClick={handleAddToCart}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </span>
               </div>
             </div>
           </div>
